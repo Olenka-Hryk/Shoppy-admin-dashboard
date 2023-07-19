248e278a5d50ff6e159f9917d2fe5817e8e6bf2e
Olena Hryk <olena.hryk@gmail.com>
2023-07-19 17:10:57 +0300

    FEAT (app-calendar): add functionality

--- src/components/cards/calendar/calendar.js
+++ src/components/cards/calendar/calendar.js
@@ -2,9 +2,14 @@ import { Component } from "../../../core";
 import html from "bundle-text:./calendar.html";
 import "./calendar.css";
 
+const weekDay = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+const monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+const currentDay = new Date();
+
 export class CalendarComponent extends Component {
   render() {
     this.innerHTML = html;
+    displayCurrentDate();
   }
 
   static create() {
@@ -13,3 +18,16 @@ export class CalendarComponent extends Component {
 }
 
 Component.define("calendar", CalendarComponent);
+
+
+function displayCurrentDate() {
+  document.getElementById("month").textContent = monthNames[currentDay.getMonth()];
+  document.getElementById("weekDay").textContent = `${weekDay[currentDay.getDay()]}, ${currentDay.getDate()}`;
+  updateClock();
+}
+
+function updateClock() {
+  document.getElementById("clock").textContent = new Date().toLocaleTimeString('uk');
+}
+
+setInterval(updateClock, 1000);
