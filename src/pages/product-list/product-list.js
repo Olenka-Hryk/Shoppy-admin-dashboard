265769ed89746e220e5cde50314ae89fd18404ae
Olena Hryk <olena.hryk@gmail.com>
2023-07-24 01:38:07 +0300

    FEAT (product-list): integrate table product list with API

--- src/pages/product-list/product-list.js
+++ src/pages/product-list/product-list.js
@@ -1,9 +1,15 @@
 import { Component } from "../../core";
 import html from "bundle-text:./product-list.html";
+import { Product } from "../../api";
 
 export class ProductListComponent extends Component {
   render() {
     this.innerHTML = html;
+    this.productTable = this.querySelector("app-table-product-list");
+
+    Product.getProducts({
+      page: 1,
+    }).then((res) => this.productTable.updateTable(res));
   }
 
   static create() {
