2b50fea34b36e2169af25c971da7fc10149c0dc5
Olena Hryk <olena.hryk@gmail.com>
2023-07-24 16:21:12 +0300

    FEAT (form-create-product): integrate list of categories with API

--- src/components/forms/form-create-product/form-create-product.js
+++ src/components/forms/form-create-product/form-create-product.js
@@ -1,9 +1,14 @@
 import { Component } from "../../../core";
 import html from "bundle-text:./form-create-product.html";
+import { Category } from "../../../api";
 
 export class FormCreateProductComponent extends Component {
   render() {
     this.innerHTML = html;
+    this.categoryBadge = this.querySelector("app-category-badge");
+
+    Category.getCategories()
+    .then((res) => this.categoryBadge.updateCategoryBadgeList(res));
   }
 
   static create() {
