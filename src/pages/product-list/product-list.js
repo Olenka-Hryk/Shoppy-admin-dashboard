6e5fe2e68a59543e7b3499b6a0c9e2901b534e74
Olena Hryk <olena.hryk@gmail.com>
2023-07-24 18:56:55 +0300

    FEAT (product-list): add product search by name

--- src/pages/product-list/product-list.js
+++ src/pages/product-list/product-list.js
@@ -1,14 +1,36 @@
 import { Component } from "../../core";
-import html from "bundle-text:./product-list.html";
 import { Product } from "../../api";
+import html from "bundle-text:./product-list.html";
 
 export class ProductListComponent extends Component {
   render() {
     this.innerHTML = html;
     this.productTable = this.querySelector("app-table-product-list");
 
+    this.querySelector("#input-search-product-list").addEventListener(
+      "keypress",
+      (event) => {
+        if (event.key === "Enter") {
+          this.searchProduct(event.target.value);
+        }
+      });
+
+    this.querySelector("#search-product-action").addEventListener(
+      "click",
+      () => {
+        const inputValue = this.querySelector("#input-search-product-list").value;
+        this.searchProduct(inputValue);
+      });
+
     Product.getProducts({
       page: 1,
+    }).then((res) => this.productTable.updateTable(res));
+  }
+
+  searchProduct(query) {
+    Product.getProducts({
+      page: 1,
+      query: query,
     }).then((res) => this.productTable.updateTable(res));
   }
 
