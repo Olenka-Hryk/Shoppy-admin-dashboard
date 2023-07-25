30c93799b94146370467e458e10cc4ac505de6ff
Olena Hryk <olena.hryk@gmail.com>
2023-07-25 22:27:31 +0300

    FEAT (product-list): add category-badge on page "Product list"  & search

--- src/pages/product-list/product-list.js
+++ src/pages/product-list/product-list.js
@@ -1,11 +1,13 @@
 import { Component } from "../../core";
 import { Product } from "../../api";
+import { Category } from "../../api";
 import html from "bundle-text:./product-list.html";
 
 export class ProductListComponent extends Component {
   render() {
     this.innerHTML = html;
     this.productTable = this.querySelector("app-table-product-list");
+    this.categoryBadge = this.querySelector("app-category-badge");
 
     this.querySelector("#input-search-product-list").addEventListener(
       "keypress",
@@ -25,6 +27,27 @@ export class ProductListComponent extends Component {
     Product.getProducts({
       page: 1,
     }).then((res) => this.productTable.updateTable(res));
+
+    Category.getCategories()
+      .then((res) => this.categoryBadge.updateCategoryBadgeList(res))
+      .then(() => {
+        this.querySelector("app-category-badge").addEventListener(
+          "click",
+          (event) => {
+            const badge = event.target.closest(".category-badge__item");
+            if (!badge) return;
+            $(badge).toggleClass("category-badge__item--active");
+            this.findAllActiveCategory();
+          }
+        );
+      });
+  }
+
+  findAllActiveCategory() {
+    const activeCategories = [...$("div#category-badge > span span.category-badge__item--active")]
+      .map((elem) => $(elem).data("category"));
+      Product.getProducts({ page: 1, categories: activeCategories }).then((res) =>
+        this.productTable.updateTable(res));
   }
 
   searchProduct(query) {
